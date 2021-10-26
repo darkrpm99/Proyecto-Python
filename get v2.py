f1637b9391f417feb7ceae888cf510f8adb3b12f
Rubén <70522464+darkrpm99@users.noreply.github.com>
2021-10-26 23:09:30 +0200

    Delete get v2.py

--- get v2.py
+++
@@ -1,9 +0,0 @@
-import json
-import requests
-from requests.api import request
-
-
-respuesta = requests.get('https://api.ipify.org/?format=json')
-ip = json.loads(respuesta.content)
-
-#print("Tu ip publica es" + " " + str(ip))
